9b0df54dfca4bd798a6da4faa9b7381b36c163ad
Igor Shabunin <shabunin181104@gmail.com>
2025-03-04 16:00:17 +0500

    getTODOs

--- index.js
+++ index.js
@@ -10,15 +10,29 @@ function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
+function getTODOs() { // => Array[str]
+    const filesContent = getFiles();
+    const todoComments = [];
 
-function getTODOs()  // => Array[str]
-{}
+    for (const content of filesContent) {
+        const lines = content.split('\n');
+        for (const line of lines) {
+            if (line.includes('// TODO')) {
+                todoComments.push({'value': line.trim()});
+            }
+        }
+    }
+    return todoComments;
+}
 
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            console.log(getTODOs());
+            break
         default:
             console.log('wrong command');
             break;
